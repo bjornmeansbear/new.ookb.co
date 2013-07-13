dc5dd5a77406b9335e00e0a2e565207dd8801e8c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-07-13 00:25:57 -0400

    tweaks to homepage almost done…
    for now… need to really figure out what is going on.

--- js/newookb.js
+++ js/newookb.js
@@ -58,10 +58,6 @@ $(window).scroll(function(){
 
 // Some Custom JS for the site
 // scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
-$(".scroll").click(function(event){    
-  event.preventDefault();
-  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
-});
 
 $(document).ready(function() {
   $('#array li img').each(function() {
