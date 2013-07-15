437cc751a458d0de8563e04c8d5c3c1ffd1f7ab4
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-07-14 23:12:07 -0400

    update modernizr

--- js/newookb.js
+++ js/newookb.js
@@ -1,3 +1,11 @@
+// smooth scrolling links
+// Some Custom JS for the site
+// scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
+$(".scroll").click(function(event){		
+	event.preventDefault();
+	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+});
+
 // Parallax Scrolling for the SiteName -->
 $(window).scroll(function(){
   var s = $(window).scrollTop();
