1a7cb545af83f89f2c51635d675f30aa83d185f7
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-07 16:23:53 -0400

    preparing for looping scroll plan...

--- js/ideas.js
+++ js/ideas.js
@@ -2,3 +2,15 @@ $(window).resize(function() {
   var sw = $(window).width();
   $('#widthcounter p').text(sw);
 });
+
+$(window).scroll(function(){
+
+  var s  = $(window).scrollTop();
+  var wh = $(window).height();
+  var dh = $(document).height();
+
+  if ((s+wh)==dh) {
+    $(window).scrollTop(0);
+  } else {}
+  
+});
