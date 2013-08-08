4315e600cb5dc3a9cc1725da42e4000baf774bcc
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-07 22:10:53 -0400

    idea for new kb landing page is coming along

--- js/ideas.js
+++ js/ideas.js
@@ -14,3 +14,30 @@ $(window).scroll(function(){
   } else {}
   
 });
+
+$(window).load(function(){
+  //declaring variables
+  var s  = $(window).scrollTop();
+  var wh = $(window).height();
+  var dh = $(document).height();
+  var ww = $(window).width();
+
+  $("#portrait").css("margin-top",wh+144);
+
+  if (ww >= 980){
+    $(window).scroll(function(){
+      var s  = $(window).scrollTop();
+      $("#portrait").css("-webkit-transform","translateY(" + (s*-.1) + "px)");
+      $("#intro").css("-webkit-transform","translateY(" + (s*-.25) + "px)");
+      $("#location").css("-webkit-transform","translateY(" + (s*-.2) + "px)");
+      $("#currently").css("-webkit-transform","translateY(" + (s*-.8) + "px)");
+      $("#whatelse").css("-webkit-transform","translateY(" + (s*-.4) + "px)");
+      $("#lectures").css("-webkit-transform","translateY(" + (s*-1) + "px)");
+      $("#comma").css("-webkit-transform","translateY(" + (s*-.6) + "px)");
+      $("#bjornmeansbear").css("-webkit-transform","translateY(" + (s*-.8) + "px)");
+      $("#referencelinks").css("-webkit-transform","translateY(" + (s*-1) + "px)");
+      $("#contact").css("-webkit-transform","translateY(" + (s*-2) + "px)");
+      $("footer").css("-webkit-transform","translateY(" + (s*-1) + "px)");
+    });
+  }
+});
