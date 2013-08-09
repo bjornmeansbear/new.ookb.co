78ef8fa158cad82e3266125c208f829c9d8eed0a
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-09 18:15:10 -0400

    99% there for now

--- js/ideas.js
+++ js/ideas.js
@@ -8,21 +8,21 @@ $(window).load(function() {
   var ww = $(window).width();
   if (ww >= 980){
     $("#portrait").css("margin-top",(wh-144));
-    $("#intro").css("margin-top",(-288));
+    $("#intro").css("margin-top",(-216));
 
     $(window).scroll(function(){
       var s    = $(window).scrollTop();
       var wh   = $(window).height();
       var dh   = $(document).height();
 
-      var ratio1 = wh/(dh-wh-288);
+      var ratio1 = wh/(dh-wh-444);
 
       $("#portrait").css("transform","translateY(" + (s-(s*ratio1)) + "px)");
-      $("#intro").css("transform","translateY(" + (s*.5) + "px)");
+      $("#intro").css("transform","translateY(" + (s*.55) + "px)");
       $("#location").css("transform","translateY(" + (s*.4) + "px)");
-      $("#currently").css("transform","translateY(" + (s*.3) + "px)");
+      $("#currently").css("transform","translateY(" + (s*.4) + "px)");
       $("#whatelse").css("transform","translateY(" + (s*.2) + "px)");
-      $("#lectures").css("transform","translateY(" + (s*.1) + "px)");
+      $("#lectures").css("transform","translateY(" + (s*.2) + "px)");
       $("#comma").css("transform","translateY(" + (s*-.1) + "px)");
       $("#bjornmeansbear").css("transform","translateY(" + (s*-.2) + "px)");
       $("#referencelinks").css("transform","translateY(" + (s*-.3) + "px)");
