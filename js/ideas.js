9b0c5fe3f07698658957fbb4ee158f7ae4b63ec5
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-09 18:09:44 -0400

    okay, this is a little too complicated… but its fun at least in theory

--- js/ideas.js
+++ js/ideas.js
@@ -3,34 +3,34 @@ $(window).resize(function() {
   $('#widthcounter p').text(sw);
 });
 
-$(window).load(function(){
-  //declaring variables
-  var s  = $(window).scrollTop();
+$(window).load(function() {
   var wh = $(window).height();
-  var dh = $(document).height();
   var ww = $(window).width();
-
   if (ww >= 980){
-    $("#portrait").css("margin-top",wh+144);
+    $("#portrait").css("margin-top",(wh-144));
+    $("#intro").css("margin-top",(-288));
+
     $(window).scroll(function(){
-      var s  = $(window).scrollTop();
-      var wh = $(window).height();
-      var dh = $(document).height();
-      $("#portrait").css("-webkit-transform","translateY(" + (s*-.1) + "px)");
-      $("#intro").css("-webkit-transform","translateY(" + (s*-.25) + "px)");
-      $("#location").css("-webkit-transform","translateY(" + (s*-.2) + "px)");
-      $("#currently").css("-webkit-transform","translateY(" + (s*-.8) + "px)");
-      $("#whatelse").css("-webkit-transform","translateY(" + (s*-.4) + "px)");
-      $("#lectures").css("-webkit-transform","translateY(" + (s*-1) + "px)");
-      $("#comma").css("-webkit-transform","translateY(" + (s*-.6) + "px)");
-      $("#bjornmeansbear").css("-webkit-transform","translateY(" + (s*-.8) + "px)");
-      $("#referencelinks").css("-webkit-transform","translateY(" + (s*-1) + "px)");
-      $("#contact").css("-webkit-transform","translateY(" + (s*-2) + "px)");
-      $("footer").css("-webkit-transform","translateY(" + (s*-1) + "px)");
-      if ((s+wh)==dh) {
+      var s    = $(window).scrollTop();
+      var wh   = $(window).height();
+      var dh   = $(document).height();
+
+      var ratio1 = wh/(dh-wh-288);
+
+      $("#portrait").css("transform","translateY(" + (s-(s*ratio1)) + "px)");
+      $("#intro").css("transform","translateY(" + (s*.5) + "px)");
+      $("#location").css("transform","translateY(" + (s*.4) + "px)");
+      $("#currently").css("transform","translateY(" + (s*.3) + "px)");
+      $("#whatelse").css("transform","translateY(" + (s*.2) + "px)");
+      $("#lectures").css("transform","translateY(" + (s*.1) + "px)");
+      $("#comma").css("transform","translateY(" + (s*-.1) + "px)");
+      $("#bjornmeansbear").css("transform","translateY(" + (s*-.2) + "px)");
+      $("#referencelinks").css("transform","translateY(" + (s*-.3) + "px)");
+      $("#contact").css("transform","translateY(" + (s*-.4) + "px)");
+      $("footer").css("transform","translateY(" + (s*-.5) + "px)");
+      if ((s+wh)>=dh) {
         $(window).scrollTop(0);
       } else {}
     });
   } else {}
-  
 });
