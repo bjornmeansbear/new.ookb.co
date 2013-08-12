e4ef2f829de8285e1b7d88b056da08252d6254f3
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-12 10:30:32 -0400

    tweaking idea KB bio page

--- js/ideas.js
+++ js/ideas.js
@@ -32,5 +32,37 @@ $(window).load(function() {
         $(window).scrollTop(0);
       } else {}
     });
-  } else {}
+  } else { }
+});
+
+$(window).resize(function() {
+  var wh = $(window).height();
+  var ww = $(window).width();
+  if (ww >= 980){
+    $("#portrait").css("margin-top",(wh-144));
+    $("#intro").css("margin-top",(-216));
+
+    $(window).scroll(function(){
+      var s    = $(window).scrollTop();
+      var wh   = $(window).height();
+      var dh   = $(document).height();
+
+      var ratio1 = wh/(dh-wh-444);
+
+      $("#portrait").css("transform","translateY(" + (s-(s*ratio1)) + "px)");
+      $("#intro").css("transform","translateY(" + (s*.55) + "px)");
+      $("#location").css("transform","translateY(" + (s*.4) + "px)");
+      $("#currently").css("transform","translateY(" + (s*.4) + "px)");
+      $("#whatelse").css("transform","translateY(" + (s*.2) + "px)");
+      $("#lectures").css("transform","translateY(" + (s*.2) + "px)");
+      $("#comma").css("transform","translateY(" + (s*-.1) + "px)");
+      $("#bjornmeansbear").css("transform","translateY(" + (s*-.2) + "px)");
+      $("#referencelinks").css("transform","translateY(" + (s*-.3) + "px)");
+      $("#contact").css("transform","translateY(" + (s*-.4) + "px)");
+      $("footer").css("transform","translateY(" + (s*-.5) + "px)");
+      if ((s+wh)>=dh) {
+        $(window).scrollTop(0);
+      } else {}
+    });
+  } else { }
 });
