e19629b3f3aceac29d1750a2756b1bdd442d2d91
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-09-26 18:13:02 -0400

    update links on grad checlist

--- grad/js/gpd.js
+++ grad/js/gpd.js
@@ -20,4 +20,27 @@ $(document).ready(function() {
     }
   });
 
+  //make tab activatable by URL
+
+  //trying to make the href location and tabs play along a little better...
+
+  $(".mytabs li a").on("click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr("href");
+    window.location.hash = id;
+    history.pushState(null, null, e.href);
+    $(this).tab('show');
+  });
+
+  // on load of the page: switch to the currently selected tab
+  $(window).bind("load", function() {
+    var hashy = window.location.hash;
+    if (location.hash) {
+      console.log(hashy);
+      history.pushState(null, null, hashy);
+      window.scrollTo(0, 0);
+      $('.mytabs li a[href="' + hashy + '"]').tab('show');
+    }
+  });
+
 });
