a9660ab8cf80a1c1c1fbf2928b301c41614cbb88
Kristian Bjørnard <kristian.bjornard@gmail.com>
2015-06-19 00:28:19 -0400

    designing up a cape page

---
+++ cape/js/mobile-menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('button.toggle').click(function() {
+        $('header ul').slideToggle();
+    });
+});
+
+$(window).resize(function() {
+    ww = $(window).width();
+
+    if (ww > 768) {
+        $('header ul').show();
+    } else {
+        $('header ul').hide();
+    };
+
+});
