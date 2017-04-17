9597dcebbbdc5a6ce361edff06a2e0b10cef77ed
Kristian Bjornard <kb@ookb.co>
2017-04-16 20:04:39 -0400

    devoo idea

---
+++ devoo/js/index.js
@@ -0,0 +1,108 @@
+// recipe data 
+
+var recipes = [{
+  "id": "devoo00001",
+  "title": "Fresh Tomato Homemade Trahana (Couscous) Soup",
+  "directions": "<h3 class='mt0'>For Homemade vegetable broth</h3><p>If you want to make your own vegetable broth:</p><p>Boil on low heat: 2 onions, 4 scallions, 3 leeks, 4 carrots, parsley and celery for an hour. Strain using a cheese cloth and squeeze the vegetables to get all the juice.</p><h3>For Soup</h3><p>In a large pot heat the broth, add the tomatoes and the Dimitri extra virgin olive oil. When it comes to a boil remove the pot from the heat and stir in the couscous. Return to boil  for 8-10  minutes. Add the sea salt and the fresh ground pepper to taste.</p><p>Serve in bowls with goat cheese or any type of grated cheese.</p>",
+  "ingredients": [
+    "7 cups vegetable broth homemade (or from the store no salt added)",
+    " 1/4 cup Dimitri Extra Virgin Olive Oil  (DEVOO)",
+    "1 cup trahana or whole wheat, barley, or regular couscous",
+    "6 ripe fresh tomatoes cut in half , grate them and discard the skin",
+    "Sea salt and fresh ground pepper ",
+    "Goat cheese or any type of grated cheese (optional)"
+  ]
+}, {
+  "id": "devoo00002",
+  "title": "Cheese Pies",
+  "directions": "<p>If you purchased frozen puff pastry leave it at room temperature for 2 hours before the following.</p><p>Preheat oven to 400°F</p><p>Cut the puff pastry in squares.</p><p>In a bowl combine the crumbled Greek feta cheese, add the grated Greek Kefalograviera cheese, add the cottage cheese, add the cream cheese and the 2 eggs beaten and blend them together. Add the dried mint and pepper to taste.</p><p>Layer a pan with parchment paper.</p><p>Put 1 teaspoon of the cheese mixture at the center of the square puff pastry and close to shape a triangle. Press the edges with a fork to seal. Brush with Dimitri extra virgin olive oil or (with egg wash - beat the egg yolk with 2 teaspoons of water)</p><p>Bake in 400°F for 20 to 25 minutes until golden.</p>",
+  "ingredients": [
+    "Puff pastry (homemade or frozen from the store [If you buy frozen, leave it at room temperature for 2 hours.])",
+    "1 pound crumbled Greek feta cheese",
+    "½ pound grated kefalograviera cheese (sharp Greek cheese)",
+    "½ pound cottage cheese  ",
+    "½ pound cream cheese",
+    "2 eggs for the filling + (1 egg for the egg wash optional)",
+    "1 tablespoon dried mint",
+    "3 ounces Dimitri Extra Virgin Olive Oil (DEVOO)",
+    "Pepper to taste"
+  ]
+}, {
+  "id": "devoo00003",
+  "title": "Artisan lettuce salad (Maroulosalata)",
+  "directions": "<p>Wash the lettuce. Let them dry. Cut the lettuce very fine. Peel the cucumber and cut in cubes. Wash the scallions and cut them very fine both the white and the green part. In a bowl combine the lettuce, the cucumber and the scallions. Chop the dill and add in the bowl. Season with sea salt and fresh pepper. Add the Dimitri extra virgin olive oil and the balsamic vinegar. Toss all the ingredients together.</p>",
+  "ingredients": [
+    "Variety of artisan lettuce",
+    "1 English cucumber",
+    "4 scallions",
+    "Dill",
+    "Sea salt and fresh pepper",
+    "1/2 cup Dimitri Extra Virgin Olive Oil (DEVOO)",
+    "4 Tbs 12 year  barrel aged balsamic vinegar"
+  ]
+},{
+  "id": "devoo00004",
+  "title": "Greek Oven Baked Potatoes",
+  "directions": "<p>Preheat oven to 425°F</p><p>Put the potatoes into a baking pan.</p><p>Season  with sea salt and black pepper. Add the oregano, the garlic,  the lemon juice, the Dimitri Extra Virgin Olive Oil, the water  and mix together .</p><p>Cover the pan with parchment paper and aluminum foil and bake for 40 minutes. Unwrap and bake for an additional 15 minutes until a nice golden brown crust has formed on the potatoes.</p><p>Kali Orexi!</p>",
+  "ingredients": [
+    "8 large potatoes, peeled, cut into large wedges (about 5-6 wedges per potato)",
+    "6  garlic cloves",
+    "2 cups Dimitri Extra Virgin Olive Oil OR Dimitri Lemon Infused EVOO",
+    "1 cup water",
+    "3 tablespoon dried oregano ",
+    "Juice of 2 lemons  ",
+    "Sea salt and black pepper"
+  ]
+},{
+  "id": "devoo00005",
+  "title": "Roast Leg of Lamb",
+  "directions": "<p>Bake the whole garlic for 20 minutes (375°F) on a parchment paper. Let it cool and peel. You can keep it in the refrigerator in a glass container for a month.</p><p>Preheat oven to 425°F</p><p>Cut kefalograviera cheese in little cubes.</p><p>Put the leg of lamb on a piece of parchment paper big enough to wrap it in a package. Make little holes with a sharp knife. Insert in each hole either a piece of the kefalograviera cheese or a clove of roasted garlic.</p><p>In a bowl mix together the thyme, oregano, rosemary, salt and pepper and rub generously all around the roast leg. Rub the Dimitri Lemon Infused extra virgin olive oil all around the roast leg and wrap it tightly in the parchment paper. Put in a roasting pan, add the water in the pan and cover the pan with an aluminum foil.</p><p>Roast at 425°F for 20 minutes. Then reduce the heat to 350°F and roast an additional hour. Check the meat thermometer. Remove from the oven anywhere from 130°F to 135°F for medium rare. Lamb should never be cooked until well done or it will be too dry. Let stand for 15-20 minutes before carving. Collect the juice from the parchment paper in the roasting pan. While the roast is resting, use a metal spatula to scrape up the drippings in the roasting pan. Use the drippings to make gravy, or use just the drippings to serve with the lamb.</p><p>Cut away the kitchen string and slice with a sharp carving knife, 1/2 inch thick slices.</p>",
+  "ingredients": [
+    "1 leg of lamb, boneless. tied up with kitchen string by butcher ",
+    "10 cloves roasted garlic",
+    "½ pound kefalograviera cheese (sharp Greek cheese)",
+    "2 teaspoons of fresh thyme or 1 teaspoon of dried thyme",
+    "2 Tbsp of fresh chopped rosemary or 1 Tbsp of dried rosemary",
+    "2 Tbsp of oregano",
+    "1/4 cup Dimitri Lemon Infused Extra Virgin Olive Oil",
+    "Salt and pepper",
+    "½ cup water",
+    "Parchment paper",
+    "Aluminum foil"
+  ]
+},{
+  "id": "devoo00006",
+  "title": "Tzatziki",
+  "directions": "<p>Peel and grate the cucumber. Add a little salt and squeeze between the hands to remove all the water. Put the yoghurt in a bowl, add the cucumber, add the Dimitri Extra Virgin Olive Oil (DEVOO) and  mix everything together until it is all blended. Add the red wine vinegar and the dill.  Add the garlic (optional). Add pepper and taste for seasoning.  Add salt if you think it’ s necessary.Refrigerate 1 / 2 hour before you serve.</p>",
+  "ingredients": [
+    "1 pint  Greek yoghurt FAGE",
+    "1 English cucumber",
+    "1/3 cup Dimitri Extra Virgin Olive Oil (DEVOO)",
+    "1 tablespoon Greek red wine vinegar “TOP”",
+    "1 tablespoon minced fresh dill",
+    "3 garlic cloves, mashed to a paste",
+    "salt and pepper"
+  ]
+}];
+
+var recipeListItem = '{{#.}}<li><a class="title" href="#{{id}}" data-cardId="{{id}}">{{title}}</a></li>{{/.}}';
+
+var recipeCard = '{{#.}}<li id="{{id}}" class="card pa3 border-yellow br3 ba bw4"><h1>{{title}}</h1><div class="flex"><ul class="ingredients w-40 list ma0 mb4 pa0 pr3"><h4 class="ma0 mb1 gray">Ingredients:</h4>{{#ingredients}}<li class="bt pt2 pb2 f6 border-light-green">{{.}}</li>{{/ingredients}}</ul><div class="description w-60 pa0 pl3 mb4">{{{directions}}}</div></li>{{/.}}';
+
+var recipeList  = Mustache.render(recipeListItem, recipes);
+var recipeCards = Mustache.render(recipeCard, recipes);
+
+$("#recipeList").html(recipeList);
+$("#recipeCards").html(recipeCards);
+
+$(".card").first().toggle();
+$("#recipeList li").first().addClass("active");
+
+$("#recipeList a").on("click", function() {
+  event.preventDefault();
+  var card = $(this).attr("href");
+  $("a.title").parent().removeClass("active");
+  $(this).parent().addClass("active");
+  $("#recipeCards .card").hide();
+  $("#recipeCards").find(card).toggle();
+});
